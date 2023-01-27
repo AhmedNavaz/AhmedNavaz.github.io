65d6c4518148308618a42fa0ec6bf6eb1524b169
Ahmed Nawaz <83880002+AhmedNavaz@users.noreply.github.com>
2023-01-27 15:15:07 +0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,8 +55,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5368f7b4b82fb3fc19f486237bf6eb28",
-"/": "5368f7b4b82fb3fc19f486237bf6eb28",
+"index.html": "9b5ccc475d4028b52fa1bbdc5aa83edd",
+"/": "9b5ccc475d4028b52fa1bbdc5aa83edd",
 "main.dart.js": "9c64a707afdc99caffa74f321caa9680",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"
